1a0aec43d569413f0ee01d8e151817509e8c54dd
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-11-01 19:48:24 -0700

    Started doing the API Controllers

---
+++ GoPillBox/Controllers/AlarmEventsController.cs
@@ -0,0 +1,56 @@
+﻿using GoPillBox.Models;
+using GoPillBox.Models.ViewModels;
+using GoPillBox.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoPillBox.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlarmEventsController : ControllerBase
+    {
+        private readonly IAlarmEventRepository _alarmEventRepository;
+
+        public AlarmEventsController(IAlarmEventRepository alarmEventRepository)
+        {
+            this._alarmEventRepository = alarmEventRepository;
+        }
+
+        // GET: api/<AlarmEventsController>
+        [HttpGet]
+        [ProducesResponseType(typeof(AlarmEvent), 200)]
+        public async Task<IActionResult> Get()
+        {
+            List<AlarmEvent> alarEvents = await this._alarmEventRepository.ReadAllAsync();
+            return Ok(alarEvents);
+        }
+
+        // GET api/<AlarmEventsController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(AlarmEvent), 200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Get(int id)
+        {
+            AlarmEvent? alarmEvent = await this._alarmEventRepository.ReadAsync(id);
+            if (alarmEvent == null)
+                return NotFound();
+            return Ok(alarmEvent);
+        }
+
+        // POST api/<AlarmEventsController>
+        [HttpPost("{id}")]
+        [ProducesResponseType(typeof(AlarmEvent), 200)]
+        [ProducesResponseType(404)]
+        public Task<IActionResult> Post(int id, [FromBody] AlarmEventView newAlarmEvent)
+        {
+
+        }
+
+        // PUT api/<AlarmEventsController>/5
+        [HttpPut("{id}")]
+        public Task<IActionResult> Put(int id, [FromBody] string value)
+        {
+
+        }
+    }
+}
---
+++ GoPillBox/Controllers/MedicationsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GoPillBox.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicationsController : ControllerBase
+    {
+        // GET: api/<MedicationsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MedicationsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MedicationsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MedicationsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MedicationsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

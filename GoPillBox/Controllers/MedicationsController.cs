d6acb94aedfbb06a55d6fa0bd61fa5b748099a51
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-11-05 20:48:08 -0700

    Added the DbContext with only the DbSets

--- GoPillBox/Controllers/MedicationsController.cs
+++ GoPillBox/Controllers/MedicationsController.cs
@@ -3,8 +3,6 @@ using GoPillBox.Models.ViewModels;
 using GoPillBox.Repositories.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace GoPillBox.Controllers
 {
     [Route("api/[controller]")]
---
+++ GoPillBox/Database/GoPillBoxDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GoPillBox.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoPillBox.Database
+{
+    public class GoPillBoxDbContext : DbContext
+    {
+        public GoPillBoxDbContext() { }
+        public GoPillBoxDbContext(String connectionString)
+        {
+            
+        }
+        public DbSet<AlarmEvent> AlarmEvents { get; set; }
+        public DbSet<Medication> Medications { get; set; }
+        public DbSet<MedicationInTreatment> MedicationInTreatments { get; set; }
+        public DbSet<Treatment> Treatments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}

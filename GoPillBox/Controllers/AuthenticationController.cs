fc80ba9bd9f85bda0d31399b1a2bf80ed71415fb
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-11-21 17:33:45 -0700

    Started work on Authentication controller

---
+++ GoPillBox/Controllers/AuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿using GoPillBox.Database;
+using GoPillBox.Models;
+using GoPillBox.Models.ViewModels;
+using GoPillBox.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoPillBox.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly GoPillBoxDbContext _dbContext;
+        private readonly ILoggingService _logger;
+
+        AuthenticationController(GoPillBoxDbContext dbContext, ILoggingService logger) 
+        {
+            this._dbContext = dbContext;
+            this._logger = logger;
+        }
+
+        // POST: api/<AuthenticationController>/Authenticate/
+        [HttpPost]
+        [ProducesResponseType(typeof(User), 200)]
+        [ProducesResponseType(401)]
+        public IActionResult Authenticate([FromBody] UserView userCredentials)
+        {
+
+        }
+
+        // GET: api/<AuthenticationController>
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(401)]
+        public IActionResult CheckIfAuthenticated()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ GoPillBox/Services/UserAuthenticationService.cs
@@ -0,0 +1,30 @@
+﻿using GoPillBox.Database;
+using GoPillBox.Models.ViewModels;
+
+namespace GoPillBox.Services
+{
+    public class UserAuthenticationService
+    {
+        private readonly GoPillBoxDbContext _goPillBoxDbContext;
+        private readonly ILoggingService _logger;
+
+        public UserAuthenticationService(GoPillBoxDbContext goPillBoxDbContext, ILoggingService logger) 
+        {
+            this._goPillBoxDbContext = goPillBoxDbContext;
+            this._logger = logger;
+        }
+
+        public async bool AreCredentialsValid(UserView userCredentials)
+        {
+            try
+            {
+                var user = await this._goPillBoxDbContext.Users.FindAsync(userCredentials);
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogException(ex);
+                throw;
+            }
+        }
+    }
+}
